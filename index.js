ec7b1fe6ed61e8b3f6749464ce7d64954d1b0ecf
Gabriel Santos Faria <fariamg4@gmail.com>
2024-04-30 10:38:46 -0300

    resolve endpoint /login

--- index.js
+++ index.js
@@ -2,21 +2,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const User = require('./models/User');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-// Endpoint de login (vulnerável a SQL Injection)
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ where: { username, password } });
-  if (user) {
-    res.json({ message: 'Login successful', user });
-  } else {
-    res.status(401).json({ message: 'Invalid credentials' });
+  if (!username) {
+    return res.status(400).json({ message: 'Username is required' });
   }
+  const user = await User.findOne({ where: { username } });
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) {
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
+  return res.json({ message: 'Logged in' });
 });
 
 app.get('/users', async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,10 +1,47 @@
 // models/User.js
 const Sequelize = require('sequelize');
 const sequelize = require('../sequelize');
+const bcrypt = require('bcrypt');
 
 const User = sequelize.define('user', {
-  username: Sequelize.STRING,
-  password: Sequelize.STRING,
+  username: {
+    type: Sequelize.STRING,
+    unique: true,
+    validate: {
+      isAlphanumeric: {
+        args: true,
+        msg: 'The username can only contain letters and numbers',
+      },
+      len: {
+        args: [3, 20],
+        msg: 'The username must be between 3 and 20 characters long',
+      },
+      notEmpty: {
+        args: true,
+        msg: 'The username is required',
+      },
+    },
+  },
+  password: {
+    type: Sequelize.STRING,
+    unique: true,
+    validate: {
+      len: {
+        args: [8, 100],
+        msg: 'The password must be at least 8 characters long',
+      },
+      notEmpty: {
+        args: true,
+        msg: 'The password is required',
+      },
+    },
+  },
+  hooks: {
+    beforeCreate: async (user) => {
+      const hashedPassword = await bcrypt.hash(user.password, 10);
+      user.password = hashedPassword;
+    },
+  },
 });
 
 module.exports = User;
